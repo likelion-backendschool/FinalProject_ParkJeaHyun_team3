440d4dc1a4fea312890e1c734fdcb1d13e6648aa
NeoGuRi95 <jea5158@gmail.com>
2022-10-17 15:12:26 +0900

    test : MemberServiceTests 추가
    # '1명 회원 생성 테스트' 추가
    # 회원 1명이 추가되었을 때 전체 회원 수가 1 증가하는지 확인
    # 추가된 회원의 이메일이 같은 지 확인

---
+++ 1Week_Mission/src/test/java/com/ll/finalProject/MemberServiceTests.java
@@ -0,0 +1,42 @@
+package com.ll.finalProject;
+
+import com.ll.finalProject.member.entity.Member;
+import com.ll.finalProject.member.repository.MemberRepository;
+import com.ll.finalProject.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTests {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("1명 회원 생성 테스트")
+    public void t1() throws Exception {
+        //given
+        String userName = "홍길동";
+        String password = "1234";
+        String email = "test@email.com";
+
+        //when
+        List<Member> beforeMembers = memberRepository.findAll();
+        Member member = memberService.create(userName, password, email);
+        List<Member> afterMembers = memberRepository.findAll();
+
+        //then
+        assertThat(afterMembers.size()).isEqualTo(beforeMembers.size() + 1);
+        assertThat(member.getEmail()).isEqualTo("test@email.com");
+    }
+}
